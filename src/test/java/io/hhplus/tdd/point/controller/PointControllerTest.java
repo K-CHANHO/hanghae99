46a3eaedc51982b1f848acbe9397c3f1890315e4
Chanho Kim <cksgh1565@gmail.com>
2025-07-07 22:59:44 +0900

    [feat] 포인트 조회 Controller

---
+++ src/test/java/io/hhplus/tdd/point/controller/PointControllerTest.java
@@ -0,0 +1,52 @@
+package io.hhplus.tdd.point.controller;
+
+import io.hhplus.tdd.point.PointController;
+import io.hhplus.tdd.point.UserPoint;
+import io.hhplus.tdd.point.service.PointService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+public class PointControllerTest {
+
+    @InjectMocks
+    private PointController pointController;
+
+    @Mock
+    private PointService pointService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setMockMvc(){
+        mockMvc = MockMvcBuilders.standaloneSetup(pointController).build();
+    }
+
+    @Test
+    void viewPoint() throws Exception {
+        // given
+        String url = "/point/1";
+        when(pointService.viewPoint(1)).thenReturn(new UserPoint(1, 2000, System.currentTimeMillis()));
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+        );
+
+        // then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.point").value(2000));
+    }
+}
