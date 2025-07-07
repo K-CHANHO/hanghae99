e217afbce8b053ee6c1850ed7d0370523b264f4d
Chanho Kim <cksgh1565@gmail.com>
2025-07-07 22:15:12 +0900

    [feat] 포인트 조회 Service

---
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -0,0 +1,34 @@
+package io.hhplus.tdd.point.service;
+
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.UserPoint;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PointServiceTest {
+
+    @Mock
+    UserPointTable userPointTable;
+
+    @InjectMocks
+    PointService pointService;
+
+    @Test
+    void viewPoint(){
+        // given
+        long userId = 1;
+        Mockito.when(userPointTable.selectById(userId)).thenReturn(new UserPoint(0,0,0));
+
+        // when
+        UserPoint userPoint = pointService.viewPoint(userId);
+
+        // then
+        Assertions.assertThat(userPoint).isNotNull();
+    }
+}
