d58d1acae85d05900d8ac351f73fb683aa7d992a
Chanho Kim <cksgh1565@gmail.com>
2025-07-11 01:27:19 +0900

    [refactor] 동시성 제어

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
@@ -17,31 +19,49 @@ public class PointService {
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
-    public UserPoint viewPoint(long userId) {
+    private ConcurrentHashMap<Long, ReentrantLock> lockMap = new ConcurrentHashMap<>();
 
+    public UserPoint viewPoint(long userId) {
         return userPointTable.selectById(userId);
     }
 
-    public synchronized UserPoint chargePoint(long userId, long chargeAmount) throws Exception {
-        UserPoint userPoint = userPointTable.selectById(userId);
+    public UserPoint chargePoint(long userId, long chargeAmount) throws Exception {
+        ReentrantLock lock = lockMap.computeIfAbsent(userId, reentrant -> new ReentrantLock());
 
-        long chargedPoint = userPoint.point() + chargeAmount;
-        if(chargedPoint > 100000) throw new Exception("포인트는 10만원을 넘길 수 없습니다.");
+        try {
+            lock.lock();
 
-        saveHistory(userId, chargeAmount, TransactionType.CHARGE, System.currentTimeMillis());
+            UserPoint userPoint = userPointTable.selectById(userId);
 
-        return userPointTable.insertOrUpdate(userId, chargedPoint);
+            long chargedPoint = userPoint.point() + chargeAmount;
+            if (chargedPoint > 100000) throw new Exception("포인트는 10만원을 넘길 수 없습니다.");
+
+            saveHistory(userId, chargeAmount, TransactionType.CHARGE, System.currentTimeMillis());
+
+            return userPointTable.insertOrUpdate(userId, chargedPoint);
+        } finally {
+            lock.unlock();
+        }
+
+
     }
 
     public UserPoint usePoint(long userId, long amount) throws Exception {
-        UserPoint userPoint = userPointTable.selectById(userId);
+        ReentrantLock lock = lockMap.computeIfAbsent(userId, reentrant -> new ReentrantLock());
 
-        long afterPoint = userPoint.point() - amount;
-        if(afterPoint < 0) throw new Exception("포인트가 부족합니다.");
+        try {
+            lock.lock();
 
-        saveHistory(userId, amount, TransactionType.USE, System.currentTimeMillis());
+            UserPoint userPoint = userPointTable.selectById(userId);
+            long afterPoint = userPoint.point() - amount;
+            if (afterPoint < 0) throw new Exception("포인트가 부족합니다.");
 
-        return userPointTable.insertOrUpdate(userId, afterPoint);
+            saveHistory(userId, amount, TransactionType.USE, System.currentTimeMillis());
+
+            return userPointTable.insertOrUpdate(userId, afterPoint);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public PointHistory saveHistory(long userId, long amount, TransactionType type, long updateMillis) {
--- src/test/java/io/hhplus/tdd/point/service/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceIntegrationTest.java
@@ -1,11 +1,16 @@
 package io.hhplus.tdd.point.service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
-import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -16,15 +21,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
 public class PointServiceIntegrationTest {
 
-    @Autowired
+    //@Autowired
     PointService pointService;
+
+    @BeforeEach
+    void initData() throws Exception {
+        UserPointTable userPointTable = new UserPointTable();
+        PointHistoryTable pointHistoryTable = new PointHistoryTable();
+        pointService = new PointService(userPointTable, pointHistoryTable);
+
+        pointService.chargePoint(1,80000);
+        pointService.usePoint(1, 10000);
+    }
 
     @Test
     @DisplayName("포인트 조회")
-    @Order(1)
     void viewPoint(){
         // given
         long userId = 1;
@@ -34,30 +48,29 @@ public class PointServiceIntegrationTest {
 
         // then
         assertThat(userPoint).isNotNull();
+        assertThat(userPoint.point()).isEqualTo(70000);
     }
 
     @Test
     @DisplayName("포인트 충전")
-    @Order(2)
     void chargePoint() throws Exception {
         // given
         long userId = 1;
-        long chargeAmount = 20000;
+        long chargeAmount = 10000;
 
         // when
         UserPoint userPoint = pointService.chargePoint(userId, chargeAmount);
 
         // then
-        assertThat(userPoint.point()).isEqualTo(20000);
+        assertThat(userPoint.point()).isEqualTo(80000);
     }
 
     @Test
     @DisplayName("포인트는 총 10만원까지만 충전가능")
-    @Order(3)
     void chargePointUntil10만원() throws Exception {
         // given
         long userId = 1;
-        long chargeAmount = 90000;
+        long chargeAmount = 50000;
 
         // when, then
         assertThatThrownBy(() -> pointService.chargePoint(userId, chargeAmount))
@@ -68,7 +81,6 @@ public class PointServiceIntegrationTest {
 
     @Test
     @DisplayName("포인트 사용")
-    @Order(4)
     void usePoint() throws Exception {
         // given
         long userId = 1;
@@ -78,17 +90,16 @@ public class PointServiceIntegrationTest {
         UserPoint userPoint = pointService.usePoint(userId, amount);
 
         // then
-        assertThat(userPoint.point()).isEqualTo(10000);
-        assertThat(pointService.viewPoint(1).point()).isEqualTo(10000);
+        assertThat(userPoint.point()).isEqualTo(60000);
+        assertThat(pointService.viewPoint(1).point()).isEqualTo(60000);
     }
 
     @Test
     @DisplayName("보유중인 포인트 내에서만 사용 가능")
-    @Order(5)
     void usePointFail(){
         // given
         long userId = 1;
-        long amount = 20000;
+        long amount = 90000;
 
         // when, then
         assertThatThrownBy(() -> pointService.usePoint(userId, amount))
@@ -98,7 +109,6 @@ public class PointServiceIntegrationTest {
 
     @Test
     @DisplayName("포인트 충전/사용 내역 조회")
-    @Order(6)
     void viewPointChargeAndUseHistory(){
         // given
         long userId = 1;
@@ -109,7 +119,7 @@ public class PointServiceIntegrationTest {
         // then
         assertThat(pointHistoryList.isEmpty()).isFalse();
         assertThat(pointHistoryList.size()).isEqualTo(2);
-        assertThat(pointHistoryList.get(0).amount()).isEqualTo(20000);
+        assertThat(pointHistoryList.get(0).amount()).isEqualTo(80000);
         assertThat(pointHistoryList.get(0).type()).isEqualTo(TransactionType.CHARGE);
         assertThat(pointHistoryList.get(1).amount()).isEqualTo(10000);
         assertThat(pointHistoryList.get(1).type()).isEqualTo(TransactionType.USE);
@@ -118,7 +128,6 @@ public class PointServiceIntegrationTest {
 
     @Test
     @DisplayName("포인트 충전 동시성 테스트")
-    @Order(7)
     void chargePointConcurrentTest() throws InterruptedException {
         // given
         // 스레드 풀 생성
@@ -140,7 +149,34 @@ public class PointServiceIntegrationTest {
 
         // then
         countDownLatch.await();
-        assertThat(pointService.viewPoint(1).point()).isEqualTo(20000);
+        assertThat(pointService.viewPoint(1).point()).isEqualTo(80000);
+
+    }
+
+    @Test
+    @DisplayName("포인트 사용 동시성 테스트")
+    void usePointConcurrentTest() throws InterruptedException {
+        // given
+        // 스레드 풀 생성
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        int executeCount = 100;
+        CountDownLatch countDownLatch = new CountDownLatch(executeCount);
+
+        // when
+        for(int i=0; i<executeCount; i++){
+            executorService.submit(() -> {
+                try {
+                    UserPoint userPoint = pointService.usePoint(1, 100);
+                    countDownLatch.countDown();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        // then
+        countDownLatch.await();
+        assertThat(pointService.viewPoint(1).point()).isEqualTo(60000);
 
     }
 
